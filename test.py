a273a33bec100b4da3b76286a9ebf9b14ae15b64
Francesco Evangelista <francesco.evangelista@gmail.com>
2016-10-29 17:04:07 -0400

    Last commit before namechange.

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ T2 = pw.make_operator("T2",["oo->vv"])
 T3 = pw.make_operator("T3",["ooo->vvv"])
 T4 = pw.make_operator("T4",["oooo->vvvv"])
 
-T = pw.make_operator("T",["o->v","oo->vv","ooo->vvv","oooo->vvvv"])
+T = pw.make_operator("T",["o->v","oo->vv","ooo->vvv","oooo->vvvv","ooooo->vvvvv"])
 F = pw.make_operator("F", ["o->o","o->v","v->o","v->v"]);
 V = pw.make_operator("V", ["oo->oo", "oo->ov", "oo->vv",
                            "ov->oo", "ov->ov", "ov->vv",
@@ -29,7 +29,7 @@ wdt = pw.WDiagTheorem()
 
 Hbar = pw.bch_series(H,T,4)
 
-sum = wdt.contract_sum(pw.rational(1,1),Hbar, 0, 8);
+sum = wdt.contract_sum(pw.rational(1,1),Hbar, 0, 10);
 
 eqs = sum.to_manybody_equation("R")
 
