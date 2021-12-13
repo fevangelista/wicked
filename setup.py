fd69b9f06d0aa6275e3a604eed9974b91b459565
Francesco Evangelista <francesco.evangelista@gmail.com>
2021-12-13 11:25:24 -0500

    Play some more with setup.py

--- setup.py
+++ setup.py
@@ -61,9 +61,13 @@ class CMakeBuild(build_ext):
             self.distribution.get_version())
         if not os.path.exists(self.build_temp):
             os.makedirs(self.build_temp)
+        print(" ".join(['cmake', ext.sourcedir] + cmake_args))
+        print(" ".join(['cmake', '--build', '.'] + build_args))
+       
         subprocess.check_call(['cmake', ext.sourcedir] + cmake_args,
                               cwd=self.build_temp, env=env)
-        subprocess.check_call(['cmake', '--build', '.'] + build_args)
+        #subprocess.check_call(['cmake', '--build', '.'] + build_args)
+
         # Copy *_test file to tests directory
         #test_bin = os.path.join(self.build_temp, 'e_test')
         #self.copy_test_file(test_bin)
