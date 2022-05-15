8ed5a18441049b71baff5ae9b14390c90778cc7e
Francesco Evangelista <francesco.evangelista@gmail.com>
2022-05-15 16:34:16 -0400

    Add missing files

---
+++ wicked/diagrams/graph_matrix.cc
@@ -0,0 +1,154 @@
+#include <cassert>
+#include <numeric>
+
+#include "graph_matrix.h"
+#include "helpers/helpers.h"
+#include "helpers/orbital_space.h"
+
+using namespace std;
+
+GraphMatrix::GraphMatrix() {}
+
+GraphMatrix::GraphMatrix(const std::vector<int> &cre,
+                         const std::vector<int> &ann) {
+  for (int i = 0; i < osi->num_spaces(); i++) {
+    elements_[i] = std::make_pair(cre[i], ann[i]);
+  }
+}
+
+const GraphMatrix::graph_matrix_t &GraphMatrix::elements() const {
+  return elements_;
+}
+
+const std::pair<int, int> &GraphMatrix::elements(int space) const {
+  return elements_[space];
+}
+
+int GraphMatrix::cre(int space) const { return elements_[space].first; }
+
+int GraphMatrix::ann(int space) const { return elements_[space].second; }
+
+void GraphMatrix::set_cre(int space, int value) {
+  elements_[space].first = value;
+}
+
+void GraphMatrix::set_ann(int space, int value) {
+  elements_[space].second = value;
+}
+
+int GraphMatrix::num_ops(int space) const { return cre(space) + ann(space); }
+
+int GraphMatrix::num_ops() const {
+  int r = 0;
+  for (const auto &pair : elements_) {
+    r += pair.first + pair.second;
+  }
+  return r;
+}
+
+bool GraphMatrix::operator<(GraphMatrix const &other) const {
+  return elements_ < other.elements_;
+}
+
+bool GraphMatrix::operator==(GraphMatrix const &other) const {
+  return elements_ == other.elements_;
+}
+
+bool GraphMatrix::operator!=(GraphMatrix const &other) const {
+  return elements_ != other.elements_;
+}
+
+GraphMatrix &GraphMatrix::operator+=(const GraphMatrix &rhs) {
+  for (int s = 0; s < osi->num_spaces(); ++s) {
+    elements_[s].first += rhs.elements_[s].first;
+    elements_[s].second += rhs.elements_[s].second;
+  }
+  return *this;
+}
+
+GraphMatrix &GraphMatrix::operator-=(const GraphMatrix &rhs) {
+  for (int s = 0; s < osi->num_spaces(); ++s) {
+    elements_[s].first -= rhs.elements_[s].first;
+    elements_[s].second -= rhs.elements_[s].second;
+  }
+  return *this;
+}
+
+GraphMatrix GraphMatrix::adjoint() const {
+  std::vector<int> cre_v(osi->num_spaces(), 0);
+  std::vector<int> ann_v(osi->num_spaces(), 0);
+  for (int s = 0; s < osi->num_spaces(); ++s) {
+    cre_v[s] = ann(s);
+    ann_v[s] = cre(s);
+  }
+  return GraphMatrix(cre_v, ann_v);
+}
+
+std::string GraphMatrix::str() const {
+  // std::vector<std::string> cv, av;
+  // for (int s = 0; s < osi->num_spaces(); ++s) {
+  //   cv.push_back(to_string(cre(s)));
+  // }
+  // for (int s = 0; s < osi->num_spaces(); ++s) {
+  //   av.push_back(to_string(ann(s)));
+  // }
+  // return "[" + join(cv, " ") + "|" + join(av, " ") + "]";
+
+  std::vector<std::string> cv, av;
+  for (int s = 0; s < osi->num_spaces(); ++s) {
+    for (int i = 0; i < cre(s); ++i) {
+      std::string op_s(1, osi->label(s));
+      cv.push_back(op_s + "+");
+    }
+  }
+  for (int s = 0; s < osi->num_spaces(); ++s) {
+    for (int i = 0; i < ann(s); ++i) {
+      std::string op_s(1, osi->label(s));
+      cv.push_back(op_s);
+    }
+  }
+  return join(cv, " ");
+}
+
+std::ostream &operator<<(std::ostream &os, const GraphMatrix &v) {
+  os << v.str();
+  return os;
+}
+
+int sum_num_ops(const std::vector<GraphMatrix> &graph_matrices) {
+  return std::accumulate(graph_matrices.begin(), graph_matrices.end(), 0,
+                         [&](int a, const auto &b) { return a + b.num_ops(); });
+}
+
+std::string to_string(const std::vector<GraphMatrix> &elements_vec) {
+  // print the creation operator above
+  std::vector<std::string> lines;
+  for (int s = 0; s < osi->num_spaces(); ++s) {
+    std::string line;
+    for (const auto &graph_matrix : elements_vec) {
+      line += std::to_string(graph_matrix.cre(s)) + " " +
+              std::to_string(graph_matrix.ann(s)) + "   ";
+    }
+    lines.push_back(line);
+  }
+  return join(lines, "\n");
+}
+
+std::string signature(const GraphMatrix &graph_matrix) {
+  std::string str;
+  for (int s = 0; s < osi->num_spaces(); ++s) {
+    str += std::to_string(graph_matrix.cre(s));
+  }
+  for (int s = 0; s < osi->num_spaces(); ++s) {
+    str += to_string(graph_matrix.ann(s));
+  }
+  return str;
+}
+
+std::string signature(const std::vector<GraphMatrix> &elements_vec) {
+  std::string s;
+  for (const auto &graph_matrix : elements_vec) {
+    s += signature(graph_matrix);
+  }
+  return s;
+}
---
+++ wicked/diagrams/graph_matrix.h
@@ -0,0 +1,92 @@
+#ifndef _wicked_diag_elements_h_
+#define _wicked_diag_elements_h_
+
+#include <array>
+#include <string>
+#include <vector>
+
+#include "../wicked-def.h"
+
+/// A class to keep track of creation and annilation operators,
+/// and their contractions, which we call a graph matrix.
+/// This object stores a vector of pairs of number for each space
+class GraphMatrix {
+  // Here we use an optimized way to store the graph matrix
+  // We assume that we work at most with 8 different spaces, which
+  // should be enough for virtually any type of application
+  static constexpr int max_spaces_ = 8;
+  using graph_matrix_t = std::array<std::pair<int, int>, max_spaces_>;
+
+public:
+  /// Constructor
+  GraphMatrix();
+
+  /// Constructor. Set number of creation and annihilation operators
+  GraphMatrix(const std::vector<int> &cre, const std::vector<int> &ann);
+
+  /// Return a vector of pairs of creation/annihilation operators
+  const graph_matrix_t &elements() const;
+
+  /// Return a pair of creation/annihilation operators in space
+  const std::pair<int, int> &elements(int space) const;
+
+  /// Return the number of creation operators in space
+  int cre(int space) const;
+
+  /// Return the number of annihilation operators in space
+  int ann(int space) const;
+
+  /// Set the number of creation operators in space
+  void set_cre(int space, int value);
+
+  /// Set the number of annihilation operators in space
+  void set_ann(int space, int value);
+
+  /// Return the number of creation + annilation operators represented by this
+  /// graph matrix
+  int num_ops() const;
+
+  /// Return the number of creation + annilation operators represented by this
+  /// graph matrix in a given space
+  int num_ops(int space) const;
+
+  /// Comparison operators used for sorting
+  bool operator<(GraphMatrix const &other) const;
+  bool operator==(GraphMatrix const &other) const;
+  bool operator!=(GraphMatrix const &other) const;
+
+  // Adds the operator count of another object
+  GraphMatrix &operator+=(const GraphMatrix &rhs);
+
+  // Subtracts the operator count of another object
+  GraphMatrix &operator-=(const GraphMatrix &rhs);
+
+  // Return the hermitian conjugate of this operator
+  GraphMatrix adjoint() const;
+
+  /// Return a string representation of the operator
+  std::string str() const;
+
+  /// Write a string representation of the operator to a stream
+  friend std::ostream &operator<<(std::ostream &os, const GraphMatrix &v);
+
+private:
+  /// This object stores the number of creation/annihilation
+  /// operators in each space. This initializes it to all zeros
+  graph_matrix_t elements_ = {};
+};
+
+// Helper functions
+
+/// Return the sum of the number creation and annihilation operator for a vector
+/// of graph matrices
+int sum_num_ops(const std::vector<GraphMatrix> &graph_matrix);
+
+/// Return a nice string representation of a vector of graph matrices
+std::string to_string(const std::vector<GraphMatrix> &elements_vec);
+
+std::string signature(const GraphMatrix &graph_matrix);
+
+std::string signature(const std::vector<GraphMatrix> &elements_vec);
+
+#endif // _wicked_diag_elements_h_
