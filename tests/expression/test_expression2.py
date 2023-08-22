6c260e2d78e7706a253e79583baaab7941fb4784
fevangelista <francesco.evangelista@emory.edu>
2023-08-22 12:03:23 -0400

    Add more tests

--- tests/expression/test_expression2.py
+++ tests/expression/test_expression2.py
@@ -1,8 +1,10 @@
 import wicked as w
-w.add_space('g','fermion','unoccupied',list('abcdefghijklmnopqrstuvwxyz'))
 
 def test_expression1():
     """Test the vacuum_normal_ordered method of the Expression class on pairs of operators"""
+    w.reset_space()
+    w.add_space('g','fermion','unoccupied',list('abcdefghijklmnopqrstuvwxyz'))
+
     # test same-type ordered
     e = w.expression("a+(g_0) a+(g_1)",False)
     expr = e.vacuum_normal_ordered()
@@ -47,6 +49,9 @@ def test_expression1():
 
 def test_expression2():
     """Test the vacuum_normal_ordered method of the Expression class on pairs of operators"""
+    w.reset_space()
+    w.add_space('g','fermion','unoccupied',list('abcdefghijklmnopqrstuvwxyz'))
+
     # test different-type ordered
     e = w.expression("a+(g_0) a-(g_1)",False)
     expr = e.vacuum_normal_ordered()
@@ -85,6 +90,27 @@ def test_expression2():
     test_expr = w.expression("-a+(g0) a-(g0)",False) + w.expression("1",False)
     assert expr == test_expr
 
+def test_expression3():
+    """Test the vacuum_normal_ordered method of the Expression class on commutators of operators"""
+    w.reset_space()
+    w.add_space('g','fermion','unoccupied',list('abcdefghijklmnopqrstuvwxyz'))
+
+    o = w.expression("a+(g_1) a-(g_2)",False)
+
+    k = w.expression("t^{}_{} a+(g_1) a-(g_3)",False)
+    k -= k.adjoint()
+
+    c1 = (o @ k) - (k @ o)
+    c1 = c1.vacuum_normal_ordered(True)
+    test_c1 = w.expression("t^{}_{} a+(g_3) a-(g_2)",False)
+    assert c1 == test_c1
+
+    c2 = (c1 @ k) - (k @ c1)
+    c2 = c2.vacuum_normal_ordered(True)
+    test_c2 = w.expression("-t^{}_{} t^{}_{} a+(g1) a-(g2)",False)
+    assert c2 == test_c2
+
 if __name__ == "__main__":
     test_expression1()
     test_expression2()
+    test_expression3()    
