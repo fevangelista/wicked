18328d593b99dbf8303481b41d475376d78b8548
Brian Zhao <zijun.zhao@emory.edu>
2024-06-24 22:25:43 -0400

    Update test, now results rigorously agree.

--- tests/diag/test_inter_general.py
+++ tests/diag/test_inter_general.py
@@ -23,7 +23,7 @@ def test_inter_general():
 def test_inter_general_numerical():
     def gen_antisym_2(n):
         rand = np.random.rand(n,n,n,n)
-
+        rand += rand.transpose(2,3,0,1)
         res = np.einsum('ijkl->ijkl', rand)
         res -= np.einsum('ijkl->jikl', rand)
         res -= np.einsum('ijkl->ijlk', rand)
@@ -32,47 +32,21 @@ def test_inter_general_numerical():
 
     def gen_antisym_3(n):
         rand = np.random.rand(n,n,n,n,n,n)
+        rand += rand.transpose(3,4,5,0,1,2)
         res = np.einsum('ijkabc->ijkabc', rand)
-        res += +1 * np.einsum("ijkabc->ijkabc", rand)
-        res += -1 * np.einsum("ijkabc->ijkacb", rand)
-        res += -1 * np.einsum("ijkabc->ijkbac", rand)
-        res += +1 * np.einsum("ijkabc->ijkbca", rand)
-        res += +1 * np.einsum("ijkabc->ijkcab", rand)
-        res += -1 * np.einsum("ijkabc->ijkcba", rand)
-        res += -1 * np.einsum("ijkabc->ikjabc", rand)
-        res += +1 * np.einsum("ijkabc->ikjacb", rand)
-        res += +1 * np.einsum("ijkabc->ikjbac", rand)
-        res += -1 * np.einsum("ijkabc->ikjbca", rand)
-        res += -1 * np.einsum("ijkabc->ikjcab", rand)
-        res += +1 * np.einsum("ijkabc->ikjcba", rand)
-        res += -1 * np.einsum("ijkabc->jikabc", rand)
-        res += +1 * np.einsum("ijkabc->jikacb", rand)
-        res += +1 * np.einsum("ijkabc->jikbac", rand)
-        res += -1 * np.einsum("ijkabc->jikbca", rand)
-        res += -1 * np.einsum("ijkabc->jikcab", rand)
-        res += +1 * np.einsum("ijkabc->jikcba", rand)
-        res += +1 * np.einsum("ijkabc->jkiabc", rand)
-        res += -1 * np.einsum("ijkabc->jkiacb", rand)
-        res += -1 * np.einsum("ijkabc->jkibac", rand)
-        res += +1 * np.einsum("ijkabc->jkibca", rand)
-        res += +1 * np.einsum("ijkabc->jkicab", rand)
-        res += -1 * np.einsum("ijkabc->jkicba", rand)
-        res += +1 * np.einsum("ijkabc->kijabc", rand)
-        res += -1 * np.einsum("ijkabc->kijacb", rand)
-        res += -1 * np.einsum("ijkabc->kijbac", rand)
-        res += +1 * np.einsum("ijkabc->kijbca", rand)
+        res += -1 * np.einsum("ijkabc->ijkcab", rand)
+        res += -1 * np.einsum("ijkabc->ijkbca", rand)
+        res += -1 * np.einsum('ijkabc->kijabc', rand)
         res += +1 * np.einsum("ijkabc->kijcab", rand)
-        res += -1 * np.einsum("ijkabc->kijcba", rand)
-        res += -1 * np.einsum("ijkabc->kjiabc", rand)
-        res += +1 * np.einsum("ijkabc->kjiacb", rand)
-        res += +1 * np.einsum("ijkabc->kjibac", rand)
-        res += -1 * np.einsum("ijkabc->kjibca", rand)
-        res += -1 * np.einsum("ijkabc->kjicab", rand)
-        res += +1 * np.einsum("ijkabc->kjicba", rand)
+        res += -1 * np.einsum("ijkabc->kijbca", rand)
+        res += -1 * np.einsum('ijkabc->jkiabc', rand)
+        res += -1 * np.einsum("ijkabc->jkicab", rand)
+        res += +1 * np.einsum("ijkabc->jkibca", rand)
         return res
 
     def gen_antisym_3_aab(n):
         rand = np.random.rand(n,n,n,n,n,n)
+        rand += rand.transpose(3,4,5,0,1,2)
         res = np.einsum('ijkabc->ijkabc', rand)
         res -= np.einsum("ijkabc->ijkbac", rand)
         res -= np.einsum("ijkabc->jikabc", rand)
@@ -81,11 +55,13 @@ def test_inter_general_numerical():
 
     def gen_antisym_3_abb(n):
         rand = np.random.rand(n,n,n,n,n,n)
+        rand += rand.transpose(3,4,5,0,1,2)
         res = np.einsum('ijkabc->ijkabc', rand)
         res -= np.einsum("ijkabc->ijkacb", rand)
         res -= np.einsum("ijkabc->ikjabc", rand)
         res += np.einsum("ijkabc->ikjacb", rand)
         return res
+
     w.reset_space()
     w.add_space('a', 'fermion', 'general', list('stuvwxyz'))
     w.add_space('A', 'fermion', 'general', list('STUVWXYZ'))
@@ -104,10 +80,10 @@ def test_inter_general_numerical():
     si_code = "def si_M(H,T,eta1,gamma1,lambda2,lambda3):\n   M = 0.0\n"
     so_code = "def so_M(H,T,eta1,gamma1,lambda2,lambda3):\n   M = 0.0\n"
     si_keys = dict()
-    for i in si_mbeq['|']:
+    for idx,i in enumerate(si_mbeq['|']):
         si_code += '   ' + w.compile_einsum(i, si_keys) + '\n'
 
-    for i in so_mbeq['|']:
+    for idx,i in enumerate(so_mbeq['|']):
         so_code += '   ' + w.compile_einsum(i) + '\n'
 
     si_code += '   return M\n'
@@ -124,20 +100,26 @@ def test_inter_general_numerical():
     lambda3_si = {}
     H_si['aaaa'] = gen_antisym_2(n)
     H_si['AAAA'] = gen_antisym_2(n)
-    H_si['aAaA'] = np.random.rand(n,n,n,n)
+    H_si['aAaA'] = gen_antisym_2(n)
     T_si['aa'] = np.random.rand(n,n)
+    T_si['aa'] += T_si['aa'].T
     T_si['AA'] = np.random.rand(n,n)
+    T_si['AA'] += T_si['AA'].T
     eta_si['aa'] = np.random.rand(n,n)
+    eta_si['aa'] += eta_si['aa'].T
     eta_si['AA'] = np.random.rand(n,n)
+    eta_si['AA'] += eta_si['AA'].T
     gamma_si['aa'] = np.random.rand(n,n)
+    gamma_si['aa'] += gamma_si['aa'].T
     gamma_si['AA'] = np.random.rand(n,n)
+    gamma_si['AA'] += gamma_si['AA'].T
     lambda2_si['aaaa'] = gen_antisym_2(n)
     lambda2_si['AAAA'] = gen_antisym_2(n)
-    lambda2_si['aAaA'] = np.random.rand(n,n,n,n)
+    lambda2_si['aAaA'] = gen_antisym_2(n)
     lambda3_si['aaaaaa'] = gen_antisym_3(n)
     lambda3_si['AAAAAA'] = gen_antisym_3(n)
-    lambda3_si['aaAaaA'] = gen_antisym_3_aab(n)
-    lambda3_si['aAAaAA'] = gen_antisym_3_abb(n)
+    lambda3_si['aaAaaA'] = gen_antisym_3(n)
+    lambda3_si['aAAaAA'] = gen_antisym_3(n)
 
     H_so = {'aaaa': np.zeros((n*2,)*4)}
     T_so = {'aa': np.zeros((n*2,)*2)}
@@ -148,21 +130,57 @@ def test_inter_general_numerical():
 
     alpha = slice(0,2*n,2)
     beta = slice(1,2*n,2)
-    sl = lambda x: alpha if x=='a' else beta
-    for k in si_keys['H']:
-        H_so['aaaa'][sl(k[0]), sl(k[1]), sl(k[2]), sl(k[3])] = H_si[k].copy()
-    for k in si_keys['T']:
-        T_so['aa'][sl(k[0]), sl(k[1])] = T_si[k].copy()
-    for k in si_keys['eta1']:
-        eta_so['aa'][sl(k[0]), sl(k[1])] = eta_si[k].copy()
-    for k in si_keys['gamma1']:
-        gamma_so['aa'][sl(k[0]), sl(k[1])] = gamma_si[k].copy()
-    for k in si_keys['lambda2']:
-        lambda2_so['aaaa'][sl(k[0]), sl(k[1]), sl(k[2]), sl(k[3])] = lambda2_si[k].copy()
-    for k in si_keys['lambda3']:
-        lambda3_so['aaaaaa'][sl(k[0]), sl(k[1]), sl(k[2]), sl(k[3]), sl(k[4]), sl(k[5])] = lambda3_si[k].copy()
 
-    assert np.isclose(si_M(H_si, T_si, eta_si, gamma_si, lambda2_si, lambda3_si), so_M(H_so, T_so, eta_so, gamma_so, lambda2_so, lambda3_so))
+    H_so['aaaa'][alpha,alpha,alpha,alpha] = H_si['aaaa'].copy()
+    H_so['aaaa'][beta,beta,beta,beta] = H_si['AAAA'].copy()
+    H_so['aaaa'][alpha,beta,alpha,beta] = H_si['aAaA'].copy()
+    H_so['aaaa'][beta,alpha,alpha,beta] = -H_si['aAaA'].copy()
+    H_so['aaaa'][alpha,beta,beta,alpha] = -H_si['aAaA'].copy()
+    H_so['aaaa'][beta,alpha,beta,alpha] = H_si['aAaA'].copy()
+
+    T_so['aa'][alpha,alpha] = T_si['aa'].copy()
+    T_so['aa'][beta,beta] = T_si['AA'].copy()
+
+    eta_so['aa'][alpha,alpha] = eta_si['aa'].copy()
+    eta_so['aa'][beta,beta] = eta_si['AA'].copy()
+
+    gamma_so['aa'][alpha,alpha] = gamma_si['aa'].copy()
+    gamma_so['aa'][beta,beta] = gamma_si['AA'].copy()
+
+    lambda2_so['aaaa'][alpha,alpha,alpha,alpha] = lambda2_si['aaaa'].copy()
+    lambda2_so['aaaa'][beta,beta,beta,beta] = lambda2_si['AAAA'].copy()
+    lambda2_so['aaaa'][alpha,beta,alpha,beta] = lambda2_si['aAaA'].copy()
+    lambda2_so['aaaa'][beta,alpha,alpha,beta] = -lambda2_si['aAaA'].copy()
+    lambda2_so['aaaa'][alpha,beta,beta,alpha] = -lambda2_si['aAaA'].copy()
+    lambda2_so['aaaa'][beta,alpha,beta,alpha] = lambda2_si['aAaA'].copy()
+
+    lambda3_so['aaaaaa'][alpha, alpha, alpha, alpha, alpha, alpha] = lambda3_si['aaaaaa'].copy()
+    lambda3_so['aaaaaa'][beta, beta, beta, beta, beta, beta] = lambda3_si['AAAAAA'].copy()
+
+    lambda3_so['aaaaaa'][alpha, alpha, beta, alpha, alpha, beta] = lambda3_si['aaAaaA'].copy()
+    lambda3_so['aaaaaa'][alpha, alpha, beta, alpha, beta, alpha] = -lambda3_si['aaAaaA'].copy()
+    lambda3_so['aaaaaa'][alpha, alpha, beta, beta, alpha, alpha] = -lambda3_si['aaAaaA'].copy()
+    lambda3_so['aaaaaa'][alpha, beta, alpha, alpha, alpha, beta] = -lambda3_si['aaAaaA'].copy()
+    lambda3_so['aaaaaa'][alpha, beta, alpha, alpha, beta, alpha] = lambda3_si['aaAaaA'].copy()
+    lambda3_so['aaaaaa'][alpha, beta, alpha, beta, alpha, alpha] = -lambda3_si['aaAaaA'].copy()
+    lambda3_so['aaaaaa'][beta, alpha, alpha, alpha, alpha, beta] = -lambda3_si['aaAaaA'].copy()
+    lambda3_so['aaaaaa'][beta, alpha, alpha, alpha, beta, alpha] = -lambda3_si['aaAaaA'].copy()
+    lambda3_so['aaaaaa'][beta, alpha, alpha, beta, alpha, alpha] = lambda3_si['aaAaaA'].copy()
+
+    lambda3_so['aaaaaa'][alpha, beta, beta, alpha, beta, beta] = lambda3_si['aAAaAA'].copy()
+    lambda3_so['aaaaaa'][alpha, beta, beta, beta, alpha, beta] = -lambda3_si['aAAaAA'].copy()
+    lambda3_so['aaaaaa'][alpha, beta, beta, beta, beta, alpha] = -lambda3_si['aAAaAA'].copy()
+    lambda3_so['aaaaaa'][beta, alpha, beta, alpha, beta, beta] = -lambda3_si['aAAaAA'].copy()
+    lambda3_so['aaaaaa'][beta, alpha, beta, beta, alpha, beta] = lambda3_si['aAAaAA'].copy()
+    lambda3_so['aaaaaa'][beta, alpha, beta, beta, beta, alpha] = -lambda3_si['aAAaAA'].copy()
+    lambda3_so['aaaaaa'][beta, beta, alpha, alpha, beta, beta] = -lambda3_si['aAAaAA'].copy()
+    lambda3_so['aaaaaa'][beta, beta, alpha, beta, alpha, beta] = -lambda3_si['aAAaAA'].copy()
+    lambda3_so['aaaaaa'][beta, beta, alpha, beta, beta, alpha] = lambda3_si['aAAaAA'].copy()
+
+    print(f'si_M = {si_M(H_si, T_si, eta_si, gamma_si, lambda2_si, lambda3_si)}')
+    print(f'so_M = {so_M(H_so, T_so, eta_so, gamma_so, lambda2_so, lambda3_so)}')
+    print(f'Ratio = {si_M(H_si, T_si, eta_si, gamma_si, lambda2_si, lambda3_si)/so_M(H_so, T_so, eta_so, gamma_so, lambda2_so, lambda3_so):.10f}')
+    assert np.isclose(si_M(H_si, T_si, eta_si, gamma_si, lambda2_si, lambda3_si)/so_M(H_so, T_so, eta_so, gamma_so, lambda2_so, lambda3_so),1.0)
 
 if __name__ == "__main__":
     test_inter_general()
